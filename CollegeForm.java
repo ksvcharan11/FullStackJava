ed532eebfe1f8bd7b0b4c88a70f547d42617613c
ksvcharan11 <113466492+ksvcharan11@users.noreply.github.com>
2025-07-24 21:41:27 +0530

    Added College form

---
+++ CollegeForm.java
@@ -0,0 +1,93 @@
+class collegeForm {
+    public static void main(String args[])
+    {
+        boolean isFirstYear = true;
+
+        boolean isTransferee = false;
+
+        boolean isReturnee = true;
+
+        String LastName = "kandimalla";
+
+        String FirstName = "Chakri";
+
+        String Complete_Address = "VJA";
+
+        String Date_Of_Birth = "06-11-2001";
+
+        String Citizenship = "Indian";
+
+        String Sex = "Male";
+
+        long Contact_Number = 8333905791l;
+
+        String religion = "Hindu";
+
+        String Name_Of_Spouse = "None";
+
+        long Contact_Of_Spouse  = 746648374674l;
+
+        String Name = "appa rao";
+
+        String Occupation = "professor";
+
+        String Guardians_Name = "abc";
+
+        String Guardians_Address = "HYD";
+
+        String High_School = "Sri Chaitanya";
+
+        short Year_Graduated = 2024;
+
+        String Last_School_Attended = "ghi";
+
+        String Course = "PCM";
+
+        short Last_School_Year = 2022;
+
+        boolean isAlsParser = false;
+
+        boolean isFourPsbeneficiary = true;
+
+        boolean isPwdDisable = false;
+
+        short Year_Taken = 2025;
+
+        int Id_Number = 202465;
+
+        String Type_Of_Disability = "No disable";
+
+        float Average = 86.5f;
+
+        System.out.println(isFirstYear);
+        System.out.println(isTransferee);
+        System.out.println(isReturnee );
+        System.out.println("The LastName = " +LastName);
+        System.out.println("The FirstName = " +FirstName);
+        System.out.println("The Address = " +Complete_Address);
+        System.out.println("The Date of birth = " +Date_Of_Birth );
+        System.out.println("The Citizenship = " +Citizenship);
+        System.out.println("The gender = " +Sex);
+        System.out.println("The Contact number = " +Contact_Number);
+        System.out.println("The name of a Spouse = " +Name_Of_Spouse);
+        System.out.println("The Contact number of a Spouse = " +Contact_Of_Spouse);
+        System.out.println(("The religion = " +religion));
+        System.out.println("The Name = " +Name);
+        System.out.println("The Occupation = " +Occupation);
+        System.out.println("The Guardians Name = " +Guardians_Name);
+        System.out.println("The Guardians address = " +Guardians_Address);
+        System.out.println("The high school name = " +High_School);
+        System.out.println("The graduated year = " +Year_Graduated);
+        System.out.println("The Last School Attended = " +Last_School_Attended);
+        System.out.println("The Course selected = " +Course);
+        System.out.println("The Last year school = " +Last_School_Year);
+        System.out.println(isAlsParser );
+        System.out.println(isFourPsbeneficiary);
+        System.out.println((isPwdDisable));
+        System.out.println("The year taken = " +Year_Taken);
+        System.out.println("Te Id number taken = " +Id_Number);
+        System.out.println("The disability type = " +Type_Of_Disability);
+        System.out.println("Te average = " +Average);
+
+    }
+}
